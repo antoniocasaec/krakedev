411e2e4002c05a595b7bdce4a0c1c82ff6b95f9a
antoniocasaec <antoniocasa.ec@gmail.com>
2025-10-18 22:18:04 -0500

    Funcion lanzarDados Dados

--- DadosArchivos/dados.js
+++ DadosArchivos/dados.js
@@ -1,5 +1,11 @@
 jugar = function (){
-    let aleatorio = Math.random();
-    console.log(aleatorio);
-    //console.log("Iniciando..");
+    lanzarDados();
+}
+
+lanzarDados = function(){
+   let aleatorio = Math.random();
+   let numeroMultiplicado = aleatorio*6;
+   let numeroEntero = parseInt(numeroMultiplicado)+1;
+   console.log(numeroEntero); 
+   return numeroEntero;
 }
