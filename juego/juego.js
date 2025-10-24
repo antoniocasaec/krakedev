4509d50794f322b0966f44a33abe45f6c667a6ab
antoniocasaec <antoniocasa.ec@gmail.com>
2025-10-23 20:44:37 -0500

    Juego piedro, papel o tijera

--- juego/juego.js
+++ juego/juego.js
@@ -1,16 +1,24 @@
+//variables para recuperar los valores de las cajas de texto
+let puntosUsuario = 0;
+let puntosComputador = 0;
+let partida = 0;
+
 jugar = function (p_seleccionado) {
     //variables para recuperar los valores de las cajas de texto
-
-    mostrarTexto("lblSeleccion", p_seleccionado);
+    //mostrarTexto("lblSeleccion", p_seleccionado);
     let v_seleccionado = p_seleccionado;
     let v_generarElemento;
     let v_generarRuta;
     let v_determinarGanador;
 
-    let erroresEstructura;
-    let v_provincia;
-    let v_tipo;
-    let v_dia;
+
+    if (puntosUsuario == 5 || puntosComputador == 5) {
+        puntosUsuario = 0;
+        puntosComputador = 0;
+    }
+
+    partida = partida + 1;
+    mostrarTexto("lblPartida", partida);
 
     v_generarElemento = generarElemento();
     console.log("v_generarElemento: " + v_generarElemento);
@@ -18,71 +26,46 @@ jugar = function (p_seleccionado) {
     v_generarRuta = generarRuta(v_generarElemento);
     console.log("generarRuta: " + generarRuta);
 
-    v_determinarGanador = determinaGanador(v_generarElemento, v_seleccionado);
+    mostrarImagen("lblimgNoImagen", v_generarRuta);
+
+    v_determinarGanador = determinaGanador(v_seleccionado, v_generarElemento);
     console.log("v_determinarGanador: " + v_determinarGanador);
 
-    if (v_determinarGanador) {
-
-
-        if (v_determinarGanador == "0") {
-
-            console.log("0 empate");
-
-        } else if (p_jugador1 > p_jugador2) {
-
-            resultado = "1";
-            console.log("1 gana jugador 1");
-
-
-        } else if (p_jugador1 < p_jugador2) {
-
-            resultado = "2";
-            console.log("2 gana jugador 2");
-
-        } else {
-            console.log("Valor errado");
-        }
-
-
-
+    if (v_determinarGanador == 0) {
+        console.log("0 empate");
+        mostrarTexto("lblquiengano", "0 empate");
+    } else if (v_determinarGanador == 1) {
+        console.log("1 Ganaste la partida!!!");
+        mostrarTexto("lblquiengano", "1 Ganaste la partida!!!");
+        puntosUsuario = v_determinarGanador + 1;
+        mostrarTexto("lblUsuario", puntosUsuario);
+    } else if (v_determinarGanador == 2) {
+        resultado = "2";
+        console.log("2 Perdiste la partida!!!");
+        mostrarTexto("lblquiengano", "2 Perdiste la partida!!!");
+        puntosComputador = puntosComputador + 1;
+        mostrarTexto("lblComputador", puntosComputador);
     }
 
+    if (puntosUsuario == 5) {
+        mostrarTexto("lblquiengano", "HAS GANADO EL JUEGO");
+    }
 
-    if (erroresEstructura == null) {
-        mostrarTexto("lblPlaca", "ESTRUCTURA VALIDA");
-        mostrarTexto("lblErrores", "N/A");
+    if (puntosComputador == 5) {
+        mostrarTexto("lblquiengano", "EL COMPUTADOR TE HA GANADO");
+    }
 
-        v_provincia = validarProvincia(primeraLetra);
-        mostrarTexto("lblProvincia", v_provincia);
+    limpiar = function () {
+        // mostrarTextoEnCaja("txtPlaca", "");
 
-        v_tipo = optenerTipoVehiculo(segundaLetra);
-        mostrarTexto("lblTipo", v_tipo);
+        //mostrarTextoEnCaja("txtPorcentajeDescuento", "");
+        mostrarTexto("lblUsuario", "0");
+        mostrarTexto("lblComputador", "0");
+        mostrarTexto("lblPartida", "0");
+        mostrarTexto("lblquiengano", "");
+        mostrarImagen("lblimgNoImagen", "./imagenes/notas.png");
 
-        v_dia = obtenerDiaPicoYPlaca(ultimaaLetra);
-        mostrarTexto("lblDiaPico", v_dia);
-
-    } else {
-        mostrarTexto("lblPlaca", "ESTRUCTURA INCORRECTA");
-        mostrarTexto("lblErrores", erroresEstructura);
-        mostrarTexto("lblProvincia", "");
+        //mostrarImagen(idComponente, rutaImagen);
     }
 }
-
-limpiar = function () {
-    /*
-        Dejar todas las cajas de texto con el valor cadena vacía, 0 ó 0.0 según el tipo de dato
-        Dejar todos los textos de los montos con el valor 0.0
-        Si funciona, hacer un commit
-     */
-    mostrarTextoEnCaja("txtPlaca", "");
-
-    //mostrarTextoEnCaja("txtPorcentajeDescuento", "0");
-    mostrarTexto("lblPlaca", "");
-    mostrarTexto("lblErrores", "N/A");
-    mostrarTexto("lblProvincia", "");
-    mostrarTexto("lblDiaPico", "");
-
-
-}
-
 /* SI TODO FUNCIONA, HACER UN PUSH */
--- juego/serviciosJuego.js
+++ juego/serviciosJuego.js
@@ -4,90 +4,62 @@ obtenerAleatorio = function () {
     let numeroEntero = parseInt(numeroMultiplicado) + 1;
     console.log(numeroEntero);
     return numeroEntero;
-}
+} // OK
 
 generarElemento = function () {
     let aleatorio = obtenerAleatorio();
     let resultado;
-
     if (aleatorio == 1) {
-
         resultado = "Piedra";
     } else if (aleatorio == 2) {
-
         resultado = "Papel";
     } else if (aleatorio == 3) {
-
-        resultado = "Tijjera";
+        resultado = "Tijera";
     } else {
-
         resultado = "Elemento Invalido";
     }
     return resultado;
-}
+} // OK
 
 determinaGanador = function (p_jugador1, p_jugador2) {
     let resultado;
     //    cambiarTexto("lblNumero", aleatorio);
-
-
- (p_jugador1 === "piedra" && p_jugador2 === "tijera") ||
-    (p_jugador1 === "papel" && p_jugador2 === "piedra") ||
-    (p_jugador1 === "tijera" && p_jugador2 === "papel")
-) {
-    resultado = 1; 
-    return resultado;
-  } if else {
-    resultado = 2; 
-    return resultado;
-  } if 
-
-
-
-
-
-    if (p_jugador1 == p_jugador2) {
-
-        resultado = "0";
+    if (p_jugador1 === p_jugador2) {
+        resultado = 0;
         console.log("0 empate");
 
-    } else if (p_jugador1 > p_jugador2) {
-
-        resultado = "1";
+    } else if  ((p_jugador1 === "Piedra" && p_jugador2 === "Tijera") ||
+        (p_jugador1 === "Papel" && p_jugador2 === "Piedra") ||
+        (p_jugador1 === "Tijera" && p_jugador2 === "Papel")) {
+        resultado = 1;
         console.log("1 gana jugador 1");
 
-
-    } else if (p_jugador1 < p_jugador2) {
-
-        resultado = "2";
+    } else {
+        resultado = 2;
         console.log("2 gana jugador 2");
 
-    } else {
-        console.log("Valor errado");
-    }
+    } //OK
 
     return resultado;
-}
 
+}
 
 generarRuta = function (p_imagen) {
     let resultado;
     //    cambiarTexto("lblNumero", aleatorio);
-    if (p_imagen == "Piedra") {
+    if (p_imagen === "Piedra") {
         resultado = "./imagenes/piedra.png";
         console.log("./imagenes/piedra.png");
 
-    } else if (p_imagen == "Papel") {
+    } else if (p_imagen === "Papel") {
         resultado = "./imagenes/papel.png";
         console.log("./imagenes/papel.png");
 
 
-    } else if (p_imagen == "Tijera") {
+    } else if (p_imagen === "Tijera") {
         resultado = "./imagenes/tijera.png";
         console.log("./imagenes/tijera.png");
 
-    } else {
-        console.log("Valor errado");
-    }
+    } 
     return resultado;
-}
+} //OK
